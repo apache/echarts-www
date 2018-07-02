363bc20b78e33d968b08667c003ba69e1ab4ee22
Ovilia <zwl.sjtu@gmail.com>
2018-07-02 11:29:46 +0800

    changes for apache

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var less        = require('gulp-less');
-var prefix      = require('gulp-autoprefixer');
-var jade        = require('gulp-jade');
-var clean       = require('gulp-clean');
-var uglify      = require('gulp-uglify');
-var copy        = require('gulp-copy');
-var jade        = require('gulp-jade');
-var fontmin     = require('gulp-fontmin');
-var replace     = require('gulp-replace');
-var config      = require('./config/env');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var less = require('gulp-less');
+var prefix = require('gulp-autoprefixer');
+var jade = require('gulp-jade');
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var copy = require('gulp-copy');
+var jade = require('gulp-jade');
+var fontmin = require('gulp-fontmin');
+var replace = require('gulp-replace');
+var config = require('./config/env');
 
 var requirejs = require('requirejs');
 
@@ -19,7 +19,7 @@ config.homeVersion = +new Date();
 /**
  * Build files for release
  */
-gulp.task('build', function(){
+gulp.task('build', function () {
     gulp.run('jade');
     gulp.run('sass');
 });
@@ -31,12 +31,14 @@ gulp.task('sass', function () {
     return gulp.src('_scss/main.scss')
         .pipe(sass({
             includePaths: ['scss'],
-            onError: function(e) {
+            onError: function (e) {
                 console.error(e);
             },
             outputStyle: 'compressed'
         }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true
+        }))
         .pipe(gulp.dest('css'));
 });
 
@@ -51,12 +53,14 @@ gulp.task('less', function () {
         ])
         .pipe(less({
             paths: ['vendors'],
-            onError: function(e) {
+            onError: function (e) {
                 console.error(e);
             },
             outputStyle: 'compressed'
         }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true
+        }))
         .pipe(gulp.dest('css'));
 });
 
@@ -69,22 +73,69 @@ gulp.task('sourceVersion', function () {
         .pipe(replace(/(urlArgs:\s*\'v=)([0-9\.]+)\'/, '$1' + config.version + '\''))
         .pipe(gulp.dest('builder/'));
 });
+
+gulp.task('copy-doc-cn', function () {
+    return gulp.src(['../echarts-doc/public/cn/documents/cn/**/*'], {
+            base: '../echarts-doc/public/cn/documents'
+        })
+        .pipe(gulp.dest('./documents'));
+});
+
+gulp.task('copy-doc-en', function () {
+    return gulp.src(['../echarts-doc/public/en/documents/en/**/*'], {
+            base: '../echarts-doc/public/en/documents'
+        })
+        .pipe(gulp.dest('./documents'));
+});
+
+gulp.task('copy-doc', ['copy-doc-cn', 'copy-doc-en']);
+
 /**
  * Generate site using Jade
  */
-gulp.task('jade', function() {
-    return gulp.src('_jade/*.jade')
+gulp.task('jade', ['copy-doc-en'], function () {
+    return gulp.src('_jade/**/*.jade')
         .pipe(jade({
             data: config
         }))
         .pipe(gulp.dest('.'));
 });
 
+gulp.task('apache', ['jade', 'sass', 'less'], function () {
+    gulp.src(['css/**/*', 'js/**/*', 'vendors/**/*', 'images/**/*', 'dist/**/*', 'video/**/*'], {
+            base: '.'
+        })
+        .pipe(gulp.dest('../incubator-echarts-website'));
+
+    gulp.src(['en/*.html'])
+        .pipe(gulp.dest('../incubator-echarts-website'));
+
+    gulp.src([
+            '../echarts-doc/public/en/*.html',
+            '!../echarts-doc/public/en/index.html'
+        ])
+        .pipe(gulp.dest('../incubator-echarts-website'));
+
+    gulp.src([
+            '../echarts-doc/public/en/documents/**/*',
+            '!../echarts-doc/public/en/documents/en/*.html',
+            '../echarts-doc/public/en/js/hm.js'
+        ], {
+            base: '../echarts-doc/public/en'
+        })
+        .pipe(gulp.dest('../incubator-echarts-website'));
+
+    gulp.src(['../echarts-doc/public/en/css/font/**/*'], {
+            base: '../echarts-doc/public/en/css'
+        })
+        .pipe(gulp.dest('../incubator-echarts-website/css'));
+});
+
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files
  */
-gulp.task('watch', ['sass', 'less', 'jade'], function() {
+gulp.task('watch', ['sass', 'less', 'jade'], function () {
     gulp.watch('_scss/*.scss', ['sass']);
     gulp.watch([
         'js/docTool/ecOption.less',
@@ -182,17 +233,23 @@ gulp.task('release-copy', ['sass', 'less', 'jade', 'release-clean'], function ()
         .pipe(copy('release'));
 });
 
-gulp.task('release-clean', function() {
-    return gulp.src(['./release'], {read: false})
-        .pipe(clean({force: true}));
+gulp.task('release-clean', function () {
+    return gulp.src(['./release'], {
+            read: false
+        })
+        .pipe(clean({
+            force: true
+        }));
 });
 
 gulp.task('release', ['release-docJS', 'release-spreadsheetJS', 'release-otherJS', 'release-copy']);
 
-gulp.task('release-doc-web', ['less'], function() {
+gulp.task('release-doc-web', ['less'], function () {
     return gulp.src([
             './js/docTool/*', './vendors/dt/**', './css/ecOption.css', './images/**'
-        ], {base: './'})
+        ], {
+            base: './'
+        })
         .pipe(gulp.dest('../echarts-doc/public/cn'))
         .pipe(gulp.dest('../echarts-doc/public/en'));
 });
