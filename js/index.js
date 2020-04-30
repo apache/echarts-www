f7140da98f3ce2bff8b801cdfe09a763f28725e6
Ovilia <zwl.sjtu@gmail.com>
2020-04-30 14:41:09 +0800

    feat: index video play on click instead of auto

--- js/index.js
+++ js/index.js
@@ -93,7 +93,24 @@ function renderHomepage3TouchDemo(echarts) {
 
 }
 
-function playIndexVideo() {
-    var video = document.getElementById('video-index');
-    video.play();
+function playVideo(id) {
+    var video = document.getElementById(id);
+    video && video.play();
+
+    var playBtn = document.getElementById(id + '-play');
+    playBtn && (playBtn.style.display = 'none');
+
+    var pauseBtn = document.getElementById(id + '-pause');
+    pauseBtn && (pauseBtn.style.display = 'block');
+}
+
+function pauseVideo(id) {
+    var video = document.getElementById(id);
+    video && video.pause();
+
+    var playBtn = document.getElementById(id + '-play');
+    playBtn && (playBtn.style.display = 'block');
+
+    var pauseBtn = document.getElementById(id + '-pause');
+    pauseBtn && (pauseBtn.style.display = 'none');
 }
