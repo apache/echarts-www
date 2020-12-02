ca206e9f21d07a143e913114a1bf82afea9d63d2
100pah <sushuang0322@gmail.com>
2020-12-02 22:48:57 +0800

    update download version to 5.0.0

--- js/download.js
+++ js/download.js
@@ -1,6 +1,10 @@
 // $.getJSON("https://api.github.com/repos/apache/incubator-echarts/releases").done(function (param) {
     // `yyyy-MM-dd` should be correct. `hh:mm:ss` doesn't matter.
     var param = [{
+        publishedAt: '2020-12-02T00:00:00Z',
+        prerelease: false,
+        name: '5.0.0'
+    }, {
         publishedAt: '2020-09-01T00:00:00Z',
         prerelease: false,
         name: '4.9.0'
