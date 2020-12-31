b80e6744e26e7af6e7a324d250723c384b2a06ab
Ovilia <zwl.sjtu@gmail.com>
2020-12-31 16:53:03 +0800

    new home page design

--- js/index.js
+++ js/index.js
@@ -3,45 +3,6 @@
         return;
     }
     document.getElementById('nav-index').className = 'active';
-    var defaultEle = $('.navbar-default');
-    defaultEle.addClass('navbar-bg');
-
-    $('#page-index').scroll(function () {
-        if ($('#page-index')[0].scrollTop > 600) {
-            defaultEle.removeClass('navbar-bg');
-        }
-        else {
-            defaultEle.addClass('navbar-bg');
-        }
-    });
-
-    var charts = null;
-
-    resize();
-    $(window).resize(resize);
-
-    function resize() {        // home video
-        var video = document.getElementById('video-index');
-        if (window.innerWidth / window.innerHeight < 16 / 9) {
-            video.style.height = window.innerHeight + 'px';
-            video.style.width = 'auto';
-            video.style.marginLeft = Math.floor((window.innerWidth
-                - window.innerHeight / 9 * 16) / 2) + 'px';
-            video.style.marginTop = 0;
-        }
-        else {
-            video.style.width = window.innerWidth + 'px';
-            video.style.height = 'auto';
-            video.style.marginTop = Math.floor((window.innerHeight
-                - window.innerWidth / 16 * 9)) + 'px';
-            video.style.marginLeft = 0;
-        }
-        if (charts) {
-            for (var i = charts.length - 1; i >= 0; --i) {
-                charts[i].resize();
-            }
-        }
-    }
 })();
 
 function renderHomepage3TouchDemo(echarts) {
