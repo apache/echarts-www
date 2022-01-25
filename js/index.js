a55b4dae0f91c49eb1e42b118a1a1e53005a43be
pissang <bm2736892@gmail.com>
2022-01-25 09:38:33 +0800

    optimize responsive

--- js/index.js
+++ js/index.js
@@ -122,8 +122,8 @@ window.lazyLoadOptions = {
 
     function loadLandingAnimationScripts() {
         return new Promise((resolve) => {
-            // var landingAnimationURL = 'http://localhost/echarts-www-landing-animation/'
-            var landingAnimationURL = 'https://cdn.jsdelivr.net/npm/echarts-www-landing-animation/';
+            var landingAnimationURL = 'http://localhost/echarts-www-landing-animation/'
+            // var landingAnimationURL = 'https://cdn.jsdelivr.net/npm/echarts-www-landing-animation/';
             // Load script.
             var link = document.createElement('link');
             var script = document.createElement('script');
