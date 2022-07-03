24ecf120f35bcf10783e117a06b37b61cc68882b
pissang <bm2736892@gmail.com>
2022-07-03 11:28:01 +0800

    switch to apache cdn in zh site

--- config/env.asf.js
+++ config/env.asf.js
@@ -6,13 +6,15 @@ config.host = 'https://echarts.apache.org';
 config.cdnPayRootMap = {
     // Expensive!!! use it carefully.
     // zh: 'https://echarts-www.cdn.bcebos.com', // origin: 'https://echarts-www.bj.bcebos.com'
-    zh: 'https://fastly.jsdelivr.net/gh/apache/echarts-website@asf-site',
+    // zh: 'https://fastly.jsdelivr.net/gh/apache/echarts-website@asf-site',
+    zh: 'https://echarts.apache.org',
     en: 'https://echarts.apache.org'
 };
 config.cdnFreeRootMap = {
     // 'echarts.cdn.apache.org' have been configured for zh (?)
     // zh: 'https://echarts.cdn.apache.org',
-    zh: 'https://fastly.jsdelivr.net/gh/apache/echarts-website@asf-site',
+    // zh: 'https://fastly.jsdelivr.net/gh/apache/echarts-website@asf-site',
+    zh: 'https://echarts.apache.org',
     en: 'https://echarts.apache.org'
 };
 config.galleryPath = 'https://echarts.apache.org/examples/';
