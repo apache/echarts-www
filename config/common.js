add3a5c217184fb87730b0f5f05c745296a2cd54
plainheart <yhen@all-my-life.cn>
2024-03-17 00:29:12 +0800

    change staticfile CDN domain

--- config/common.js
+++ config/common.js
@@ -23,7 +23,7 @@ module.exports = {
     },
 
     cdnThirdPartyCN: {
-        jquery: 'https://cdn.staticfile.org/jquery/3.7.1/jquery.min.js',
+        jquery: 'https://cdn.staticfile.net/jquery/3.7.1/jquery.min.js',
         bootstrapCSS: 'https://lib.baomitu.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css',
         bootstrapJS: 'https://lib.baomitu.com/twitter-bootstrap/3.3.7/js/bootstrap.min.js',
         lodash: 'https://lib.baomitu.com/lodash.js/3.10.1/lodash.min.js',
